3cce7328e8a78d4f5cdc303501acd5fd0396d2a4
Swetha18105 <ss7776@srmist.edu.in>
2026-04-24 19:51:37 +0530

    connected to render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/bfhl", {
+      const res = await fetch("https://bfhl-project-yyw3.onrender.com/bfhl", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
